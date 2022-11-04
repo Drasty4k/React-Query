fffde179837e99e50a4a25c921fbc82e6810ba90
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-11-04 19:52:38 +0200

    set up the react query provider and client and fetched some data using the useQuery hook

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,23 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchedSuperHerouse = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const { data, isLoading } = useQuery("super-heroes", fetchedSuperHerouse);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => (
+        <div key={hero.id}>{hero.name}</div>
+      ))}
+    </>
+  );
+};
