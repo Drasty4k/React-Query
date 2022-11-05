5f9cd4fa59f6c53fe53f7dbe90640e39ebf46763
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-11-05 20:08:04 +0200

    Mutations implementation

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import {
+  useSuperHeroesData,
+  useAddSuperHeroData,
+} from "../hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
+
   const onSuccess = (data) => {
     console.log("Perfom side effect after data fetching", data);
   };
@@ -12,10 +19,19 @@ export const RQSuperHeroesPage = () => {
 
   const { data, isLoading, isError, error, isFetching, refetch } =
     useSuperHeroesData(onSuccess, onError);
+
+  const { mutate: addHero } = useAddSuperHeroData();
+
   console.log({
     isLoading,
     isFetching,
   });
+
+  const handleAddHeroClick = () => {
+    console.log({ name, alterEgo });
+    const hero = { name, alterEgo };
+    addHero(hero);
+  };
 
   if (isLoading) {
     return <h2>Loading...</h2>;
@@ -28,6 +44,19 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={(e) => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
       <button onClick={refetch}>Fetch heroes</button>
       {data?.data.map((hero) => (
         <div key={hero.id}>
